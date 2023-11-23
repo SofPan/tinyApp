59839257bdc17e5cd04f1f953eb8b23a0981ea16
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-23 11:45:14 -0500

    clear user_id cookie on logout

--- express_server.js
+++ express_server.js
@@ -105,13 +105,11 @@ app.post("/urls/:id/update", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  // const username = req.body.username;
-  // res.cookie("username", username);
   res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
-  // res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
