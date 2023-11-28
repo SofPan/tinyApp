4dc098587a51a63940a14c6f532810f47d68ad18
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-28 11:37:36 -0500

    add method-override node module

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const morgan = require('morgan');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
@@ -42,6 +43,7 @@ const users = {
 
 /* ----- MIDDLEWARE ----- */
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(morgan('dev'));
 app.use(cookieSession({
   name: 'session',
