2388bd1a545f210aa267902c1f720f8b64e8f594
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-28 11:50:49 -0500

    add unit tests for urlsForUserID function

--- test/helpersTests.js
+++ test/helpersTests.js
@@ -15,6 +15,17 @@ const testUsers = {
   }
 };
 
+const testURLs = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2RandomID"
+  },
+};
+
 describe("#getUserByEmail", () => {
   it("should return a user with a valid email", () => {
     const user = getUserByEmail("user@example.com", testUsers);
@@ -24,6 +35,23 @@ describe("#getUserByEmail", () => {
   it("should return undefined when a user is not in our database", () => {
     const invalidUser = getUserByEmail("notfound@example.com", testUsers);
     assert.strictEqual(invalidUser, undefined);
+  });
+});
+
+describe("#urlsForUserID", () => {
+  it("should return an object containing only the user's URLs", () => {
+    const expected = {
+      "b2xVn2": {
+        longURL: "http://www.lighthouselabs.ca",
+        userID: "userRandomID"
+      },
+    };
+    const actual = urlsForUserID("userRandomID", testURLs);
+    assert.deepStrictEqual(actual, expected);
+  });
+  it("should return an empty object when no URLs match user ID", () => {
+    const noURLsMatch = urlsForUserID("notAndID", testURLs);
+    assert.deepStrictEqual(noURLsMatch, {});
   });
 });
 
